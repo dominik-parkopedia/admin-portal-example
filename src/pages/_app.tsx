54e242e212f4555acdc0896b7337e5ee9db06a4b
Dominik Lessel <dominik.lessel@parkopedia.com>
2024-04-04 13:59:18 +0200

    chore: title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,31 @@
-import { type AppType } from "next/app";
+import Head from "next/head";
 import { Inter } from "next/font/google";
-
 import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
 import MainNav from "~/components/main-nav";
+import "~/styles/globals.css";
+
+import { type AppType } from "next/app";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main
-      className={`bg-background min-h-screen font-sans antialiased ${inter.variable}`}
-    >
-      <div className="min-h-full">
-        <MainNav />
-        <Component {...pageProps} />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Parkopedia Admin Panel</title>
+      </Head>
+      <main
+        className={`bg-background min-h-screen font-sans antialiased ${inter.variable}`}
+      >
+        <div className="min-h-full">
+          <MainNav />
+          <Component {...pageProps} />
+        </div>
+      </main>
+    </>
   );
 };
 
